b42371cb4a791929050ed21893d35daf72d73b25
Linus Fischer <leicraft@leicraftmc.de>
2025-11-30 06:10:20 +0100

    update doc

--- src/server/store/abstractRecordStore.ts
+++ src/server/store/abstractRecordStore.ts
@@ -4,6 +4,12 @@ import { DNSRecords } from "../../utils/records";
 export abstract class AbstractDNSRecordStore {
 
     abstract getRecords(name: string, type: DNSRecords.TYPES): Promise<DNSQuery.Response>;
+
+    /**
+     * Get the authority section for a given name
+     * @param name The domain name to get authority for
+     * @returns SOA and NS records for the zone. First element should be the SOA record, than NS records, tthan others.
+     */
     abstract getAuthority(name: string): Promise<DNSRecords.ResponseWithoutClass[]>;
 
 }
