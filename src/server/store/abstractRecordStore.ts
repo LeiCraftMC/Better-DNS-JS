2e23633dec0767f00758ee6733d0bfbc2b1f4ca2
Linus Fischer <leicraft@leicraftmc.de>
2025-12-18 18:18:43 +0000

    update

--- src/server/store/abstractRecordStore.ts
+++ src/server/store/abstractRecordStore.ts
@@ -3,6 +3,11 @@ import { DNSRecords } from "../../utils/records";
 
 export abstract class AbstractDNSRecordStore {
 
+    /**
+     * Get DNS records for a given name and type
+     * @param name The domain name to get records for
+     * @param type The DNS record type to get
+     */
     abstract getRecords(name: string, type: DNSRecords.TYPES): Promise<AbstractDNSRecordStore.Types.GetRecordsResponse>;
 
     /**
@@ -12,6 +17,11 @@ export abstract class AbstractDNSRecordStore {
      */
     abstract getAuthority(name: string): Promise<AbstractDNSRecordStore.Types.GetAuthorityResponse>;
 
+    /**
+     * Get all records for a given zone
+     * @param name The zone name to get all records for
+     */
+    abstract getAllRecordsForZone(name: string): Promise<DNSQuery.Response>;
 }
 
 export namespace AbstractDNSRecordStore.Types {
